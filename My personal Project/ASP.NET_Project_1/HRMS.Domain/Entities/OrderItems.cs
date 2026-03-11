e0357af639dc271d5756eba78155f0983006d36b
Sefat33 <sefatahmed3674@gmail.com>
2026-03-11 15:55:26 +0600

    Order and OrderItems

---
+++ My personal Project/ASP.NET_Project_1/HRMS.Domain/Entities/OrderItems.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMS.Domain.Entities
+{
+    public class OrderItems
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Orders Orders { get; set; } = null!;
+
+        [Required]
+        public int SubCategoryId { get; set; }
+
+        [ForeignKey("SubCategoryId")]
+        public SubCategories SubCategories { get; set; } = null!;
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        // Calculated field
+        public decimal TotalPrice { get; set; }
+    }
+}
